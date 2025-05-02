9dc965aafcf4c4f04bb5aaed10d6b2adfcbdf99a
AbdualimovBotir <botirabdualimov07@gmail.com>
2025-05-02 09:43:35 +0500

    this is my telegram bot

---
+++ src/main/java/uz/clicktocode/bot/ClickToCodeBotApplication.java
@@ -0,0 +1,13 @@
+package uz.clicktocode.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickToCodeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickToCodeBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/clicktocode/bot/ClickToCodeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.clicktocode.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickToCodeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
